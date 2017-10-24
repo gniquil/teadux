2a49b54fcbaf1267531f9284722cd469c2490c90
Frank Liu <gniquil@gmail.com>
2017-10-24 11:57:39 -0700

    fixed export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export * from './functions';
 export { createEnhancer, createStore } from './redux';
 export { Runtime, Item } from './runtime';
 export * from './types';
-export { actionSanitizer, makeStateSanitizer } from './devtools';
+export { makeSanitizers } from './devtools';
 export { compose } from 'redux';
 export { mcompose, _composeCache } from './utils';
